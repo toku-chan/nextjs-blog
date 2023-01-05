a1947e1e9b600b01eb96c4921f26e05762221c36
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-01-05 23:06:56 +0900

    Assets, Metadata, and CSS

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import "../styles/globals.css";
+
+// _app.jsはアプリケーション内の全てのページをラップする最上位のReactコンポーネント
+// 全てのページに影響を与えることができる、また全てのページで共通の状態を保持したい時にも使える
+export default function APP ({ Component, pageProps}) {
+  return <Component {...pageProps} />
+}
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,27 @@
+import Head from "next/head";
+import Script from "next/script";
 import Link from "next/link";
+import { Layout } from "../../components/layout";
 
 export default function FistPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      {/* Scriptの詳細：https://nextjs.org/docs/basic-features/script */}
+      {/*
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload" // scriptをいつロードするか制御
+        onLoad={() => console.log(`script loaded correctly, window.FB has been populated`)} // scriptが読み込まれた直後に実行
+      />
+      */}
       <h1>First Post</h1>
       <h2>
         {/* STUDY: Next.js v12.2 以降では aタグをラップする必要はない */}
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   )
 }
