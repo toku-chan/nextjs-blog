f5a7f81f6b0e899d956fd9b449e34ef9eeb06f49
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-01-12 22:30:52 +0900

    update: vercelのお試しのために自己紹介文を更新

--- pages/index.jsx
+++ pages/index.jsx
@@ -23,7 +23,7 @@ export default function Home({
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[自己紹介]</p>
+        <p>vercelの「プッシュするごとにデプロイをプレビュー」を試すために文言の更新をする</p>
         <p>(This is a sample website - you’ll be building a site like this on{' '}<a href="https://nextjs.org/learn">our Next.js tutorial</a>.)</p>
       </section>
 
