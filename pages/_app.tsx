46e05b6fbcb0e93a55a3417603b892b120758020
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-01-12 23:23:37 +0900

    fix: JSからTSに変更 part1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import { AppProps } from "next/app";
 import "../styles/globals.css";
 
 // _app.jsはアプリケーション内の全てのページをラップする最上位のReactコンポーネント
 // 全てのページに影響を与えることができる、また全てのページで共通の状態を保持したい時にも使える
-export default function APP ({ Component, pageProps}) {
+export default function APP ({ Component, pageProps}: AppProps) {
   return <Component {...pageProps} />
-}
+};
---
+++ pages/api/hello.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log(req)
+  res.status(200).json({
+    test: 'Hello'
+  });
+};
